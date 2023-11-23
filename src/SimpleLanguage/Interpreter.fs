a67c048aa295a527c08591b1cb76eb7af5d20420
PolinaSavelyeva <lissava00@gmail.com>
2023-11-23 23:56:39 +0300

    Add conditions to interpreter

--- src/SimpleLanguage/Interpreter.fs
+++ src/SimpleLanguage/Interpreter.fs
@@ -3,42 +3,66 @@ module SimpleLanguage.Interpreter
 open System.Collections.Generic
 open SimpleLanguage.AST
 
+let parseCompareOperator (operator: CompareOperator) =
+    match operator with
+    | Equal -> (=)
+    | NotEqual -> (<>)
+    | LessThan -> (<)
+    | LessThanOrEqual -> (<=)
+    | GreaterThan -> (>)
+    | GreaterThanOrEqual -> (>=)
+
 let rec evaluateExpression (context: Dictionary<_, _>) expression =
     match expression with
     | Number number -> number
     | Mult listOfExpressions -> listOfExpressions |> List.map (evaluateExpression context) |> List.reduce (*)
-    | Add list -> list |> List.map (evaluateExpression context) |> List.reduce (+)
+    | Add listOfExpressions -> listOfExpressions |> List.map (evaluateExpression context) |> List.reduce (+)
     | Variable variableName ->
         if context.ContainsKey variableName then
             context[variableName]
         else
             failwithf $"Variable with name {variableName} not declared."
-
-let rec evaluateStatement context statement =
-    match statement with
-    | Assignment(variableName, expression) -> Some(variableName, evaluateExpression context expression)
-    | Print expression ->
-        printfn $"{evaluateExpression context expression}"
-        None
+    | Compare(left, operator, right) ->
+        evaluateExpression context left
+        |> (parseCompareOperator operator) (evaluateExpression context right)
+        |> System.Convert.ToInt32
 
 let evaluateAST (statements: AST) =
     let context = Dictionary<string, int>()
 
-    List.fold
-        (fun (context: Dictionary<_, _>) statement ->
-            let result = evaluateStatement context statement
+    let rec inner (currentAST: AST) =
 
-            match result with
-            | Some(variableName, result) ->
-                if context.ContainsKey variableName then
-                    context[variableName] <- result
+        let rec evaluateStatement context statement =
+            match statement with
+            | Assignment(variableName, expression) -> Some(variableName, evaluateExpression context expression)
+            | Print expression ->
+                printfn $"{evaluateExpression context expression}"
+                None
+            | Condition(condition, thenBranch, elseBranch) ->
+                if evaluateExpression context condition = 1 then
+                    inner thenBranch
                 else
-                    context.Add(variableName, result)
-            | None -> ()
+                    inner elseBranch
 
-            context)
-        context
-        statements
-    |> ignore
+                None
+
+        List.fold
+            (fun (context: Dictionary<_, _>) statement ->
+                let result = evaluateStatement context statement
+
+                match result with
+                | Some(variableName, result) ->
+                    if context.ContainsKey variableName then
+                        context[variableName] <- result
+                    else
+                        context.Add(variableName, result)
+                | None -> ()
+
+                context)
+            context
+            currentAST
+        |> ignore
+
+    inner statements
 
 // let evaluateProgram (program: list<char>) =
