9c806f8a6ce64fa8d4e6edbc4101d10e379f426c
PolinaSavelyeva <lissava00@gmail.com>
2023-11-23 23:56:13 +0300

    Add condition to AST

--- src/SimpleLanguage/AST.fs
+++ src/SimpleLanguage/AST.fs
@@ -1,13 +1,23 @@
 module SimpleLanguage.AST
 
+type CompareOperator =
+    | Equal
+    | NotEqual
+    | LessThan
+    | LessThanOrEqual
+    | GreaterThan
+    | GreaterThanOrEqual
+
 type Expression =
     | Number of int
     | Mult of list<Expression>
     | Add of list<Expression>
     | Variable of string
+    | Compare of left: Expression * operator: CompareOperator * right: Expression
 
 type Statement =
     | Assignment of variableName: string * expression: Expression
     | Print of Expression
+    | Condition of Expression * thenBranch: list<Statement> * elseBranch: list<Statement>
 
 type AST = list<Statement>
