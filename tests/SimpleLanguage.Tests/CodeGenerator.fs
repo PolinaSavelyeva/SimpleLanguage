b4dec5ac2584e34bd5ac0b0ec97ff02dbfe711bb
PolinaSavelyeva <lissava00@gmail.com>
2023-11-27 13:28:39 +0300

    Fix code generator test

--- tests/SimpleLanguage.Tests/CodeGenerator.fs
+++ tests/SimpleLanguage.Tests/CodeGenerator.fs
@@ -3,6 +3,7 @@ module SimpleLanguage.Tests.CodeGenerator
 open Expecto
 open Generators
 open SimpleLanguage.Parser
+open SimpleLanguage.Main
 
 [<Tests>]
 let tests =
@@ -10,24 +11,26 @@ let tests =
         "tests for code generator function"
         [ testCase "Generated code from AST is the same with the previous code"
           <| fun _ ->
+
               let code =
                   "if:true>falsethen:
-                x=true<true
-                print:x<false
-                else:
-                x=true
-                if:x==truethen:
-                x=true
-                else:
-                x=true
-                end
-                print:true
-                x=true
-                end
-                x=3+4+5
-                print:true&&false<true||false
-                "
+x=true<true
+print:x<false
+else:
+x=true
+if:x==truethen:
+x=true
+else:
+x=true
+end
+print:true
+x=true
+end
+x=3+4+5
+print:true&&false<true||false
+"
 
-              let codeCharList = code.ToCharArray() |> List.ofArray
-              let ast = snd (programParser codeCharList).Value |> generateCodeFromAST
-              Expect.equal ast code "Generated code were expected to be equal" ]
+              let ast = getProgramAST code
+              let generatedCode = generateCodeFromAST ast
+
+              Expect.equal code generatedCode "Generated code were expected to be equal" ]
